9390b55cf0db595fb72c0a9c1ef03e496bb47c74
theunhackable <psrsai001@gmail.com>
2023-08-29 00:15:01 +0530

    completed shops page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
+import Testimonials from '../components/Testimonials'
 
-const page = () => {
+const About = () => {
   return (
-    <div>
+    <main id='about-main' className='bg-primary'>
+      <header className='bg-[url(/images/about-us.webp)] w-full h-[350px] flex flex-col items-center justify-center'>
+        <h1 className='text-5xl font-bold text-primary'>ABOUT US</h1>
+      </header>
       
-    </div>
+      <section className='container mx-auto px-4'>
+        
+      </section>    
+      <Testimonials />
+    </main>
   )
 }
 
-export default page
+export default About
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Testimonials from '../components/Testimonials'
+import Products from '../components/Products'
+import ProductLine from '../lib/ProductLine'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main id='about-main' className='bg-primary'>
+    <header className='bg-[url(/images/about-us.webp)] w-full h-[350px] flex flex-col items-center justify-center'>
+      <h1 className='text-5xl font-bold text-primary'>SHOP</h1>
+    </header>
+    
+    <section className='mx-auto px-4 flex flex-col justify-center items-center'>
+      <h1 className='font-extrabold text-5xl my-16'>
+        OUR BLENDS DELIVER
+      </h1>
+      <ProductLine />
+    </section>
+    <Testimonials />
+  </main>
   )
 }
 
